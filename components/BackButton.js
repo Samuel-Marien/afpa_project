dedea2a59c8a489235469b500fcb2c3eee7a695a
samuel <timeuss.sam@gmail.com>
2021-03-17 15:31:06 +0100

    fix: font-weight

--- components/BackButton.js
+++ components/BackButton.js
@@ -1,10 +1,6 @@
 const BackButton = (props) => {
   const { buttonTitle } = props;
-  return (
-    <button className="btn btn-secondary font-weight-bold">
-      {buttonTitle}
-    </button>
-  );
+  return <button className="btn btn-secondary">{buttonTitle}</button>;
 };
 
 export default BackButton;
