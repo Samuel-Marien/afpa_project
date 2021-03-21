c62f53f4c9ca0d771c33fae300b6850e9705517c
samuel <timeuss.sam@gmail.com>
2021-03-21 21:48:56 +0100

    fix: icon design

--- components/CircleIconEg.js
+++ components/CircleIconEg.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 const CircleIconEg = (props) => {
   const { src, type, txt } = props;
   return (
-    <div className="w-25 mx-4">
+    <div className="w-25 mx-3">
       <Image
         src={src}
         className="w-75 rounded-circle"
@@ -11,7 +11,7 @@ const CircleIconEg = (props) => {
         height={150}
       />
       <p className="font-weight-bold">{type}</p>
-      <p className="font-weight-light" style={{ fontSize: '.8rem' }}>
+      <p className="font-weight-light text-left" style={{ fontSize: '.8rem' }}>
         {txt}
       </p>
     </div>
