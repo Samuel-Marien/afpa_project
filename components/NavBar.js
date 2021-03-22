899c5084c053a1113c51ad816ea58442441ffa09
samuel <timeuss.sam@gmail.com>
2021-03-22 23:51:22 +0100

    style: change navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -11,37 +11,48 @@ import { IoIosMail, IoIosSettings } from 'react-icons/io';
 
 const NavBar = (props) => {
   return (
-    <Navbar bg="dark" variant="dark" className="m-0 py-0">
+    <Navbar
+      bg="dark"
+      variant="dark"
+      className="m-0 py-0"
+      expand="lg"
+      fixed="top"
+      style={{ opacity: '.9' }}
+    >
       <Navbar.Brand>
         <Logo size="45" />
       </Navbar.Brand>
-      <Nav className="mr-auto">
-        <Link href="/">
-          <Nav.Link>Home</Nav.Link>
-        </Link>
-        <Nav.Link href="#features">
-          <IoIosMail size={25} />
-        </Nav.Link>
-        <Nav.Link href="/settingStudentPage">
-          <IoIosSettings size={25} className="text-info" />
-        </Nav.Link>
-      </Nav>
-      <Form inline>
-        <SignButton
-          href={'/selectRolePage'}
-          variant={'outline-info'}
-          className={'mr-3 p-1'}
-        >
-          Sign Up
-        </SignButton>
-        <SignButton
-          href={'/SignIn'}
-          variant={'outline-secondary'}
-          className={'p-1'}
-        >
-          Sign In
-        </SignButton>
-      </Form>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Link href="/">
+            <Nav.Link>Home</Nav.Link>
+          </Link>
+          <Nav.Link href="#features">
+            <IoIosMail size={25} />
+          </Nav.Link>
+          <Nav.Link href="/settingStudentPage">
+            <IoIosSettings size={25} className="text-info" />
+          </Nav.Link>
+        </Nav>
+
+        <Form inline>
+          <SignButton
+            href={'/selectRolePage'}
+            variant={'outline-info'}
+            className={'mr-3 p-1'}
+          >
+            Sign Up
+          </SignButton>
+          <SignButton
+            href={'/SignIn'}
+            variant={'outline-secondary'}
+            className={'p-1'}
+          >
+            Sign In
+          </SignButton>
+        </Form>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
