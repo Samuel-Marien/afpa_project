398eac8cea532c1f89561e922000f8b0b1ad52ea
samuel <timeuss.sam@gmail.com>
2021-03-22 11:05:27 +0100

    fix: bad inline attribute

--- components/ContentTitle.js
+++ components/ContentTitle.js
@@ -1,33 +1,11 @@
-import SignButton from './SignButton';
-
 const ContentTitle = (props) => {
+  const { title, txt1, txt2, option } = props;
   return (
     <div className="container text-center mt-5">
-      <h1 className="font-weight-bold">Let's build the network together</h1>
-      <p>
-        So that the alumni network meets your aspirations and your convictions,
-        it is with you that we are building it.
-      </p>
-      <p>
-        So that the Afpa community can act on the world, let's get involved
-        together!
-      </p>
-      <div inline className="mt-5">
-        <SignButton
-          href={'/selectRolePage'}
-          variant={'outline-info'}
-          className={'px-5 mr-5 shadow'}
-        >
-          Sign Up
-        </SignButton>
-        <SignButton
-          href={'/SignIn'}
-          variant={'outline-secondary'}
-          className={'px-5 shadow'}
-        >
-          Sign In
-        </SignButton>
-      </div>
+      <h1 className="font-weight-bold">{title}</h1>
+      <p className="mt-5">{txt1}</p>
+      <p>{txt2}</p>
+      {option}
     </div>
   );
 };
