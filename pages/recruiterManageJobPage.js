45d740d7cb482da7d798b7e08170c207b1548e50
samuel <timeuss.sam@gmail.com>
2021-04-04 16:48:50 +0200

    fix: overflow

--- pages/recruiterManageJobPage.js
+++ pages/recruiterManageJobPage.js
@@ -67,11 +67,16 @@ const ManageJob = (props) => {
       title="Recruiter Manage Job offer"
       pageTitle="Something new ?"
       imgUrl="/writing-1149962_1920.jpg"
-      childrenSize="w-50"
+      childrenSize="w-75"
     >
-      <JobThumbnail />
-      <JobThumbnail />
-      <JobThumbnail />
+      <div className="px-3 pb-2" style={{ height: '650px', overflow: 'auto' }}>
+        <JobThumbnail />
+        <JobThumbnail />
+        <JobThumbnail />
+        <JobThumbnail />
+        <JobThumbnail />
+        <JobThumbnail />
+      </div>
     </PageHorizontal>
   )
 }
