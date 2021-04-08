fa2a4803b077d35cdb0462b9974903550de91302
samuel <timeuss.sam@gmail.com>
2021-04-09 00:32:24 +0200

    feat: add search bar

---
+++ components/SearchBar.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
+
+const SearchBar = (props) => {
+  return (
+    <div className="w-50 mx-auto mt-5 mb-3 px-3 text-center rounded shadow-sm text-light bg-dark p-2">
+      <h2>What job are you looking for?</h2>
+      <InputGroup className="mb-3 mt-3">
+        <FormControl placeholder="Frontend, React, google..." />
+        <FormControl placeholder="Paris, London..." />
+        <DropdownButton
+          as={InputGroup.Prepend}
+          variant="info"
+          title="contrat"
+          id="input-group-dropdown-1"
+        >
+          <Dropdown.Item href="#">CDI</Dropdown.Item>
+          <Dropdown.Item href="#">CDD</Dropdown.Item>
+          <Dropdown.Item href="#">Stage</Dropdown.Item>
+          <Dropdown.Item href="#">Alternance</Dropdown.Item>
+        </DropdownButton>
+
+        <InputGroup.Append>
+          <Button variant="outline-info">search</Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </div>
+  )
+}
+
+export default SearchBar
