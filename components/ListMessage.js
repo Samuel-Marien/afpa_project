1478e1b4c371a319a19af32240403f3c94fbc3be
samuel <timeuss.sam@gmail.com>
2021-04-10 18:20:24 +0200

    feat: add message thumbnail

--- components/ListMessage.js
+++ components/ListMessage.js
@@ -2,98 +2,72 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 import Image from 'react-bootstrap/Image'
-import Badge from 'react-bootstrap/Badge'
 import Accordion from 'react-bootstrap/Accordion'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
-import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
 
 import { CgArrowRightR } from 'react-icons/cg'
 
-const MyP = (props) => {
-  const { className = 'mb-1', children } = props
-  return (
-    <p style={{ fontSize: '.8rem' }} className={className}>
-      {children}
-    </p>
-  )
-}
-
-MyP.propTypes = {
-  className: PropTypes.string
-}
-
-MyP.propTypes = {
-  children: PropTypes.node
-}
-
 const MessageThumnail = (props) => {
-  const [modalShow, setModalShow] = React.useState(false)
   const {
-    companyLogo = '/logos/logo-ibm.png',
-    city,
-    jobNumber,
+    avatarFace = '/portraits/female-20.jpeg',
+    firstName,
+    lastName,
     date,
-    keyword1,
-    keyword2,
-    keyword3,
-    typeContrat,
-    jobTitle,
-    txt
+    txt,
+    children
   } = props
 
   return (
-    <li className="bg-light mt-4 d-flex flex-column flex-md-row pr-3 pl-1 rounded shadow">
-      <div className="d-flex flex-column">
-        <Image src={companyLogo} rounded className="p-4 " />
-        <MyP className=" text-center bg-dark text-info py-2 rounded">
-          {city}
-        </MyP>
+    <li className="mt-4 d-flex  pr-3 pl-1 rounded ">
+      <div className="w-25 bg-dark p-1 d-none d-lg-block rounded">
+        <Image
+          src={avatarFace}
+          roundedCircle
+          fluid
+          className="border border-info"
+        />
       </div>
-      <div className="ml-4">
-        <div className="d-flex justify-content-between">
-          <MyP>{jobNumber}</MyP>
-          <MyP>{date}</MyP>
-        </div>
-        <div className="d-flex mb-3">
-          <Badge className="mr-1" variant="warning">
-            {keyword1}
-          </Badge>
-          <Badge className="mr-1" variant="warning">
-            {keyword2}
-          </Badge>
-          <Badge className="mr-1" variant="warning">
-            {keyword3}
-          </Badge>
-          <Badge variant="info">{typeContrat}</Badge>
+
+      <div className="ml-2 flex-column flex-sm-row d-flex border border-dark rounded px-3 py-2">
+        <div>
+          <div className="d-flex flex-sm-column flex-row mr-5">
+            <div className="w-50 p-1 d-sm-block d-lg-none rounded">
+              <Image
+                src={avatarFace}
+                roundedCircle
+                fluid
+                className="border border-info"
+              />
+            </div>
+            <div className="d-flex flex-column ml-5 ml-sm-1">
+              <h1 className="font-weight-bold mb-0">{firstName}</h1>
+              <h3 style={{ fontSize: '.9rem' }}>{lastName}</h3>
+              <p style={{ fontSize: '.8rem' }}>{date}</p>
+            </div>
+          </div>
         </div>
 
-        <div>
+        <div className="mt-2">
           <div style={{ fontSize: '.9rem' }}>
-            <h3>{jobTitle}</h3>
             <Accordion defaultActiveKey="1">
-              {txt.slice(0, 263)}
+              {txt.slice(0, 240)}
               <Accordion.Toggle as={Button} variant="link" eventKey="0">
                 <CgArrowRightR />
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
                 <Card.Body as="div" className="p-0">
                   {txt}
+                  {children}
                   <div className="d-flex justify-content-end">
                     <Button
                       variant="info"
                       type="submit"
                       className="w-25 mt-4 mb-1 align-self-end"
-                      onClick={() => setModalShow(true)}
                     >
                       Contact
                     </Button>
-
-                    <ResponseModal
-                      show={modalShow}
-                      onHide={() => setModalShow(false)}
-                    />
                   </div>
                 </Card.Body>
               </Accordion.Collapse>
@@ -106,108 +80,94 @@ const MessageThumnail = (props) => {
 }
 
 MessageThumnail.propTypes = {
-  companyLogo: PropTypes.string
+  avatarFace: PropTypes.string
 }
 MessageThumnail.propTypes = {
-  city: PropTypes.string
+  firstName: PropTypes.string
 }
 MessageThumnail.propTypes = {
-  jobNumber: PropTypes.string
+  lastName: PropTypes.string
 }
 MessageThumnail.propTypes = {
   date: PropTypes.string
 }
 MessageThumnail.propTypes = {
-  keyword1: PropTypes.string
-}
-MessageThumnail.propTypes = {
-  keyword2: PropTypes.string
-}
-MessageThumnail.propTypes = {
-  keyword3: PropTypes.string
-}
-MessageThumnail.propTypes = {
-  typeContrat: PropTypes.string
-}
-MessageThumnail.propTypes = {
-  jobTitle: PropTypes.string
-}
-MessageThumnail.propTypes = {
   txt: PropTypes.string
 }
+MessageThumnail.propTypes = {
+  children: PropTypes.node
+}
 
-const ResponseModal = (props) => {
-  const { src = '/logos/logo-ibm.png' } = props
+const AnswerBlock = (props) => {
+  const { txt, date, src } = props
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Image src={'/undraw_Processing_re_tbdu.svg'} style={{ opacity: '.5' }} />
-      <Modal.Header closeButton>
-        <Modal.Title
-          id="contained-modal-title-vcenter"
-          className="d-flex align-items-center"
+    <div className="border-top border-info mt-5">
+      <div className="d-flex justify-content-center">
+        <p
+          className="w-25 d-flex justify-content-center text-info"
+          style={{
+            fontSize: '.7rem',
+            transform: 'translateY(-50%)',
+            background: '#ebf0f4'
+          }}
         >
-          <Image src={src} rounded className="p-1 " />
-          <div className="ml-3">
-            <h3 className="text-secondary ">Catch this job!</h3>
-            <h6>
-              <Badge variant="info">Job-Offer: 666</Badge>
-            </h6>
-          </div>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form.Group controlId="exampleForm.ControlTextarea1">
-          <Form.Control
-            readOnly
-            plaintext
-            type="text"
-            placeholder="Object Job-Offer: 666"
-          ></Form.Control>
-          <Form.Control
-            as="textarea"
-            placeholder="Your message..."
-            rows={8}
-            className={'mt-2'}
-          />
-        </Form.Group>
-      </Modal.Body>
-      <Modal.Footer className="d-flex justify-content-between">
-        <Button variant="danger" onClick={props.onHide}>
-          Back
-        </Button>
-        <Button variant="dark">Send</Button>
-      </Modal.Footer>
-    </Modal>
+          {date}
+        </p>
+      </div>
+
+      <div className="bg-light d-flex rounded p-3">
+        <div className="w-25">
+          <Image src={src} roundedCircle fluid className="w-75" />
+        </div>
+        <div>
+          <p>{txt}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
-ResponseModal.propTypes = {
-  onHide: PropTypes.func
+AnswerBlock.propTypes = {
+  txt: PropTypes.string
 }
-ResponseModal.propTypes = {
+AnswerBlock.propTypes = {
   src: PropTypes.string
+}
+AnswerBlock.propTypes = {
+  date: PropTypes.string
 }
 
 const ListMessage = (props) => {
   return (
     <ul className=" my-5 mx-auto container container-fluid">
       <MessageThumnail
-        city={'Ville de Paris, France'}
-        jobNumber={'job-number:666'}
         date={'09.03.22'}
-        keyword1={'Frontend'}
-        keyword2={'React'}
-        typeContrat={'CDI'}
-        jobTitle={'Frontend Developper Jr'}
+        firstName={'Janne'}
+        lastName={'Doe'}
         txt={
           'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
         }
-      />
+      >
+        <AnswerBlock
+          src={'/portraits/male-5.jpg'}
+          date={'12.03.22'}
+          txt={
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem. Sed ut perspiciatis unde omnis iste natus error sit voluptatem'
+          }
+        />
+        <AnswerBlock
+          src={'/portraits/female-20.jpeg'}
+          date={'13.03.22'}
+          txt={
+            'Sed ut  Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?'
+          }
+        />
+        <AnswerBlock
+          src={'/portraits/male-5.jpg'}
+          date={'15.03.22'}
+          txt={'Sed ut perspiciatis unde  ?'}
+        />
+      </MessageThumnail>
     </ul>
   )
 }
