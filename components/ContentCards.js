6b01ff16529a1c3749a5f79207046562523449b0
samuel <timeuss.sam@gmail.com>
2021-04-15 14:56:03 +0200

    chore: add responsive

--- components/ContentCards.js
+++ components/ContentCards.js
@@ -6,47 +6,43 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 const ContentCards = (props) => {
   return (
     <Jumbotron className="mt-5">
-      <div className="d-flex justify-content-center">
-        <Thumbnail
-          src={'/portraits/male-50.jpg'}
-          title={'John Doe'}
-          role={'Recruiter'}
-          txt1={
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-          }
-        ></Thumbnail>
-        <Thumbnail
-          src={'/portraits/teacher.jpg'}
-          title={'John Doe'}
-          role={'Teacher'}
-          txt1={
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-          }
-        />
-        <Thumbnail
-          src={'/portraits/male-5.jpg'}
-          title={'John Doe'}
-          role={'Student'}
-          txt1={
-            'Hello, graduated from AFPA, I am a NodeJS / HTML developer available in Nantes'
-          }
-        />
-        <Thumbnail
-          src={'/portraits/male-6.jpg'}
-          title={'John Doe'}
-          role={'Recruiter'}
-          txt1={
-            'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
-          }
-        ></Thumbnail>
-        <Thumbnail
-          src={'/portraits/female-102.jpg'}
-          title={'John Doe'}
-          role={'Student'}
-          txt1={
-            'Hello, graduated from AFPA, I am a NodeJS / HTML developer available in Nantes'
-          }
-        />
+      <div className="d-flex flex-column flex-lg-row justify-content-center align-items-center">
+        <div className="d-flex flex-column  flex-sm-row">
+          <Thumbnail
+            src={'/portraits/male-50.jpg'}
+            title={'John Doe'}
+            role={'Recruiter'}
+            txt1={
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            }
+          ></Thumbnail>
+          <Thumbnail
+            src={'/portraits/teacher.jpg'}
+            title={'John Doe'}
+            role={'Teacher'}
+            txt1={
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            }
+          />
+        </div>
+        <div className="d-flex flex-column flex-sm-row">
+          <Thumbnail
+            src={'/portraits/male-5.jpg'}
+            title={'John Doe'}
+            role={'Student'}
+            txt1={
+              'Hello, graduated from AFPA, I am a NodeJS / HTML developer available in Nantes'
+            }
+          />
+          <Thumbnail
+            src={'/portraits/male-6.jpg'}
+            title={'John Doe'}
+            role={'Recruiter'}
+            txt1={
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+            }
+          ></Thumbnail>
+        </div>
       </div>
     </Jumbotron>
   )
--- components/ContentMiddle.js
+++ components/ContentMiddle.js
@@ -5,9 +5,13 @@ const ContentMiddle = (props) => {
   const { title, txt1, txt2, option } = props
   return (
     <div className="mx-auto my-5 d-flex flex-column w-75 align-items-center">
-      <h1 className="font-weight-bold align-self-center m-1">{title}</h1>
-      <p className="w-50 mt-5 pl-5">{txt1}</p>
-      <p className="w-50 mt-5 pl-5">{txt2}</p>
+      <h1 className="font-weight-bold align-self-center m-1 text-center">
+        {title}
+      </h1>
+
+      <p className="mt-5 text-center">{txt1}</p>
+      <p className="mt-5 text-center">{txt2}</p>
+
       {option}
     </div>
   )
