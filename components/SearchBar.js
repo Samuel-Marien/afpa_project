3aecf38281721e00c31825104214c428e4f53544
samuel <timeuss.sam@gmail.com>
2021-04-17 00:26:03 +0200

    chore: add responsive

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -8,29 +8,88 @@ import Dropdown from 'react-bootstrap/Dropdown'
 
 const SearchBar = (props) => {
   return (
-    <div className="w-50 mx-auto mt-5 mb-3 px-3 text-center rounded shadow-sm text-light bg-dark p-2">
+    <div className="container container fluid mx-auto mt-1 mt-md-5 mb-3 px-3 text-center rounded shadow-sm text-light bg-dark p-2">
       <h2>What job are you looking for?</h2>
-      <InputGroup className="mb-3 mt-3">
-        <FormControl placeholder="Frontend, React, google..." />
-        <FormControl placeholder="Paris, London..." />
-        <DropdownButton
-          as={InputGroup.Prepend}
-          variant="info"
-          title="contrat"
-          id="input-group-dropdown-1"
-        >
-          <Dropdown.Item href="#">CDI</Dropdown.Item>
-          <Dropdown.Item href="#">CDD</Dropdown.Item>
-          <Dropdown.Item href="#">Stage</Dropdown.Item>
-          <Dropdown.Item href="#">Alternance</Dropdown.Item>
-        </DropdownButton>
-
-        <InputGroup.Append>
-          <Button className="px-5" type="submit" variant="outline-info">
+      {/* first block */}
+      <div className="d-none d-md-block d-lg-none">
+        <InputGroup className="mb-3 mt-3 flex-column flex-md-row">
+          <InputGroup className="w-100">
+            <FormControl placeholder="Frontend, React, google..." />
+            <FormControl placeholder="Paris, London..." />
+            <DropdownButton
+              as={InputGroup.Prepend}
+              variant="warning"
+              title="contrat"
+              id="input-group-dropdown-1"
+            >
+              <Dropdown.Item href="#">CDI</Dropdown.Item>
+              <Dropdown.Item href="#">CDD</Dropdown.Item>
+              <Dropdown.Item href="#">Stage</Dropdown.Item>
+              <Dropdown.Item href="#">Alternance</Dropdown.Item>
+            </DropdownButton>
+          </InputGroup>
+          <Button
+            className="px-5 w-100 mt-3"
+            type="submit"
+            variant="outline-info"
+          >
             search
           </Button>
-        </InputGroup.Append>
-      </InputGroup>
+        </InputGroup>
+      </div>
+      {/* second block */}
+      <div className="d-none d-lg-block">
+        <InputGroup className="mb-3 mt-3 ">
+          <FormControl placeholder="Frontend, React, google..." />
+          <FormControl placeholder="Paris, London..." />
+          <DropdownButton
+            as={InputGroup.Prepend}
+            variant="warning"
+            title="contrat"
+            id="input-group-dropdown-1"
+          >
+            <Dropdown.Item href="#">CDI</Dropdown.Item>
+            <Dropdown.Item href="#">CDD</Dropdown.Item>
+            <Dropdown.Item href="#">Stage</Dropdown.Item>
+            <Dropdown.Item href="#">Alternance</Dropdown.Item>
+          </DropdownButton>
+          <InputGroup.Append>
+            <Button className="px-5 w-100" type="submit" variant="outline-info">
+              search
+            </Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </div>
+      {/* third block */}
+      <div className="d-block d-md-none">
+        <InputGroup className="mb-3 mt-3">
+          <FormControl
+            placeholder="Frontend, React, google..."
+            className="w-100 mb-2"
+          />
+          <FormControl placeholder="Paris, London..." />
+          <DropdownButton
+            as={InputGroup.Prepend}
+            variant="warning"
+            title="contrat"
+            id="input-group-dropdown-1"
+          >
+            <Dropdown.Item href="#">CDI</Dropdown.Item>
+            <Dropdown.Item href="#">CDD</Dropdown.Item>
+            <Dropdown.Item href="#">Stage</Dropdown.Item>
+            <Dropdown.Item href="#">Alternance</Dropdown.Item>
+          </DropdownButton>
+          <div className="w-100">
+            <Button
+              className="px-5 w-100 mt-2"
+              type="submit"
+              variant="outline-info"
+            >
+              search
+            </Button>
+          </div>
+        </InputGroup>
+      </div>
     </div>
   )
 }
--- components/SearchBarMessage.js
+++ components/SearchBarMessage.js
@@ -6,6 +6,8 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
+import { BiSearchAlt } from 'react-icons/bi'
+
 const BoardMessage = (props) => {
   const {
     totalMessage = 10,
@@ -15,22 +17,22 @@ const BoardMessage = (props) => {
   } = props
 
   return (
-    <div className="shadow p-3 rounded m-3 w-100">
+    <div className="container col-12 col-md-3 d-flex flex-wrap justify-content-between d-md-block shadow rounded border-left border-info">
       <div className="d-flex justify-content-between">
-        <p>My messages</p>
-        <p>{totalMessage}</p>
+        <p>Total</p>
+        <p className="ml-2">{totalMessage}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Student</p>
-        <p>{studentMessage}</p>
+        <p className="ml-2"> {studentMessage}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Teacher</p>
-        <p>{teacherMessage}</p>
+        <p className="ml-2">{teacherMessage}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Recruiter</p>
-        <p>{recruiterMessage}</p>
+        <p className="ml-2">{recruiterMessage}</p>
       </div>
     </div>
   )
@@ -50,23 +52,25 @@ BoardMessage.propTypes = {
 
 const SearchBarMessage = (props) => {
   return (
-    <div className="w-50 d-flex mx-auto rounded shadow-sm text-light bg-dark p-2">
-      <div>
-        <BoardMessage />
-      </div>
-      <div className="ml-5 w-75 shadow p-3 rounded m-3">
+    <div className="container container-fluid d-flex flex-column align-items-between justify-content-around flex-md-row mx-auto rounded shadow-sm text-light bg-dark p-4">
+      <BoardMessage />
+
+      <div className="ml-0 ml-md-4 shadow p-3 w-100 rounded border-left border-info">
         <h2>Mailboxes</h2>
-        <InputGroup>
-          <InputGroup className="mb-3 mt-3">
-            <FormControl placeholder="What are you looking for?" />
-            <InputGroup.Append>
-              <Button className="px-5" type="submit" variant="outline-info">
-                search
-              </Button>
-            </InputGroup.Append>
-          </InputGroup>
+
+        <InputGroup className="mb-3 mt-3">
+          <FormControl
+            className="col-12"
+            placeholder="What are you looking for?"
+          />
+          <InputGroup.Append>
+            <Button className="" type="submit" variant="outline-info">
+              <BiSearchAlt role="button" />
+            </Button>
+          </InputGroup.Append>
         </InputGroup>
-        <InputGroup className="w-75 d-flex justify-content-between align-items-start p-0">
+
+        <InputGroup className="d-flex justify-content-between align-items-start p-0">
           <Form.Check type="checkbox" label="All" id="" />
           <Form.Check type="checkbox" label="Student" id="" />
           <Form.Check type="checkbox" label="Alumni" id="" />
