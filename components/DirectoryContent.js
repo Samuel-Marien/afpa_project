083c1d8c1abe7271030467a292ade187bf6c0c94
samuel <timeuss.sam@gmail.com>
2021-04-18 17:45:00 +0200

    fix: adjust responsive comportement

--- components/DirectoryContent.js
+++ components/DirectoryContent.js
@@ -7,7 +7,7 @@ import Thumbnail from './Thumbnail'
 const DirectoryContent = (props) => {
   return (
     <Jumbotron variant="bg-danger" className="mt-5">
-      <div className="d-flex justify-content-center flex-wrap align-items-between px-5">
+      <div className="d-flex justify-content-center flex-wrap align-items-between px-0 px-md-5">
         <Thumbnail
           src={'/portraits/male-50.jpg'}
           title={'John Doe'}
