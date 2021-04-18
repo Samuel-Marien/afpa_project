6522a3e1308d8ab7674aaa8de7107108b6c5d0e1
samuel <timeuss.sam@gmail.com>
2021-04-18 15:24:35 +0200

    chore: add responsive

--- components/SearchBarNetworks.js
+++ components/SearchBarNetworks.js
@@ -6,67 +6,66 @@ import FormControl from 'react-bootstrap/FormControl'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 
+import { BiSearchAlt } from 'react-icons/bi'
+
 const BoardMessage = (props) => {
-  const {
-    totalMessage = 10,
-    studentMessage = 6,
-    teacherMessage = 3,
-    recruiterMessage = 1
-  } = props
+  const { totalContacts = 10, student = 6, teacher = 3, recruiter = 1 } = props
 
   return (
-    <div className="shadow p-3 rounded m-3 w-100">
+    <div className="container col-12 col-md-3 d-flex flex-wrap justify-content-between d-md-block shadow rounded border-left border-info">
       <div className="d-flex justify-content-between">
         <p>All my contacts</p>
-        <p>{totalMessage}</p>
+        <p className="ml-2">{totalContacts}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Student</p>
-        <p>{studentMessage}</p>
+        <p className="ml-2">{student}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Teacher</p>
-        <p>{teacherMessage}</p>
+        <p className="ml-2">{teacher}</p>
       </div>
       <div className="d-flex justify-content-between">
         <p>Recruiter</p>
-        <p>{recruiterMessage}</p>
+        <p className="ml-2">{recruiter}</p>
       </div>
     </div>
   )
 }
 BoardMessage.propTypes = {
-  totalMessage: PropTypes.number
+  totalContacts: PropTypes.number
 }
 BoardMessage.propTypes = {
-  studentMessage: PropTypes.number
+  student: PropTypes.number
 }
 BoardMessage.propTypes = {
-  teacherMessage: PropTypes.number
+  teacher: PropTypes.number
 }
 BoardMessage.propTypes = {
-  recruiterMessage: PropTypes.number
+  recruiter: PropTypes.number
 }
 
 const SearchBarNetworks = (props) => {
   return (
-    <div className="w-50 d-flex mx-auto rounded shadow-sm text-light bg-dark p-2">
-      <div>
-        <BoardMessage />
-      </div>
-      <div className="ml-5 w-75 shadow p-3 rounded m-3">
+    <div className="col-12 col-md-8 container container-fluid d-flex flex-column align-items-between justify-content-around flex-md-row mx-auto rounded shadow-sm text-light bg-dark p-4">
+      <BoardMessage />
+
+      <div className="ml-0 ml-md-4 shadow p-3 w-100 rounded border-left border-info">
         <h2>Your networks</h2>
-        <InputGroup>
-          <InputGroup className="mb-3 mt-3">
-            <FormControl placeholder="What are you looking for?" />
-            <InputGroup.Append>
-              <Button className="px-5" type="submit" variant="outline-info">
-                search
-              </Button>
-            </InputGroup.Append>
-          </InputGroup>
+
+        <InputGroup className="mb-3 mt-3">
+          <FormControl
+            className="col-12"
+            placeholder="What are you looking for?"
+          />
+          <InputGroup.Append>
+            <Button className="" type="submit" variant="outline-info">
+              <BiSearchAlt role="button" />
+            </Button>
+          </InputGroup.Append>
         </InputGroup>
-        <InputGroup className="w-75 d-flex justify-content-between align-items-start p-0">
+
+        <InputGroup className="d-flex justify-content-between align-items-start p-0">
           <Form.Check type="checkbox" label="All" id="" />
           <Form.Check type="checkbox" label="Student" id="" />
           <Form.Check type="checkbox" label="Alumni" id="" />
